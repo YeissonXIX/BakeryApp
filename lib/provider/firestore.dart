821bea7d9b77eb18a4a7efe8b18ec27d025a7546
YeissonXIX <ja04844@gmail.com>
2021-04-08 22:35:12 -0600

    primer commit

--- lib/provider/firestore.dart
+++ lib/provider/firestore.dart
@@ -3,12 +3,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FirestoreService {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  Future<void> anadirPasteles(String desc, String name, String price) {
+  Future<void> anadirPasteles(String nombre, String apellido, String contrasena,
+      String confirmecontrasena) {
     CollectionReference users =
-        FirebaseFirestore.instance.collection('pasteles');
+        FirebaseFirestore.instance.collection('usuarios');
     // Call the user's CollectionReference to add a new user
     return users
-        .add({'descripcion': desc, 'nombre': name, 'precio': price})
+        .add({
+          'nombre': nombre,
+          'apellido': apellido,
+          'contrasena': contrasena,
+          'confirme_contrasena': confirmecontrasena
+        })
         .then((value) => print("User Added"))
         .catchError((error) => print("Failed to add user: $error"));
   }
